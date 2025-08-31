0e45a7e3c94ae9cab1c5703842f862c58e340fa7
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-08-31 12:02:37 -0300

    Criando classe abstrata Conteudo, para diminuir a repetição de código em Mentoria e Curso

---
+++ src/Conteudo.java
@@ -0,0 +1,23 @@
+public abstract class Conteudo {
+    protected static final int XP_PADRAO = 10;
+    private String titulo;
+    private String descricao;
+
+    public abstract double calcularXP();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
